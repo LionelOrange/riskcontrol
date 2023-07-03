f10cac7c263dbf60b7cf78734b79e1067983a9f6
chencheng <787915859@qq.com>
2023-07-03 17:26:05 +0800

    注释添加

--- src/main/java/com/example/riskcontrol/service/DimensionService.java
+++ src/main/java/com/example/riskcontrol/service/DimensionService.java
@@ -36,6 +36,17 @@ public class DimensionService {
 
 
     /**
+     * 这段代码是一个Lua脚本，它执行以下步骤：
+     *  1. 如果传入的第一个参数大于0，则从Redis的有序集合中删除分数在0到该参数之间的所有成员，并设置该有序集合的过期时间为传入的第二个参数。
+     * 2. 向Redis的有序集合中添加一个新成员，其分数为传入的第三个参数，成员值为传入的第四个参数。
+     * 3. 返回Redis有序集合中分数在传入的第五个参数和第六个参数之间的成员数量。
+     *  代码的详细解释如下：
+     * 1. 定义一个名为luaScript的常量，其值为一个字符串，包含Lua脚本的代码。
+     * 2. 第一行代码使用tonumber函数将传入的第一个参数转换为数字类型，并与0进行比较。如果大于0，则执行后续代码。这一行代码使用了Lua的条件语句if。
+     * 3. 第二行代码使用Redis的ZREMRANGEBYSCORE命令从有序集合中删除分数在0到传入的第一个参数之间的所有成员。该命令需要两个参数，第一个参数为有序集合的键名，第二个参数为分数的范围。这一行代码使用了Lua的字符串连接符号+。
+     * 4. 第三行代码使用Redis的EXPIRE命令设置有序集合的过期时间为传入的第二个参数。该命令需要两个参数，第一个参数为有序集合的键名，第二个参数为过期时间（以秒为单位）。这一行代码也使用了Lua的字符串连接符号+。
+     * 5. 第四行代码使用Redis的ZADD命令向有序集合中添加一个新成员。该命令需要三个参数，第一个参数为有序集合的键名，第二个参数为新成员的分数，第三个参数为新成员的值。这一行代码同样使用了Lua的字符串连接符号+。
+     * 6. 最后一行代码使用Redis的ZCOUNT命令返回有序集合中分数在传入的第五个参数和第六个参数之间的成员数量。该命令需要三个参数，第一个参数为有序集合的键名，第二个参数为分数的下限，第三个参数为分数的上限。这一行代码同样使用了Lua的字符串连接符号+。
      * 过期SortedSet的数据
      * 过期SortedSet键
      * 添加SortedSet值
@@ -53,6 +64,14 @@ public class DimensionService {
 
     /**
      * lua 添加行为数据并获取结果
+     * @param key LOGIN_sset_operateIp.devId.mobile_127.0.0.1.xxxxxxx-xxx
+     * @param remMaxScore 计算sortedset的score
+     * @param expire 过期时间
+     * @param score 计算sortedset的score
+     * @param value 17712341234
+     * @param queryMinScore sortedset最小的score
+     * @param queryMaxScore sortedset最大的score
+     * @return
      */
     private Long runSha(String key, String remMaxScore, String expire, String score, String value, String queryMinScore, String queryMaxScore) {
         if (luaSha == null) {
@@ -76,7 +95,9 @@ public class DimensionService {
         }
 
         Date operate = event.getOperateTime();
+        //operateIp.devId.mobile
         String key1 = String.join(".", String.join(".", condDimensions), aggrDimension);
+        //String[] key2 = new String[]{"127.0.0.1","xxxxxxx-xxx"};
         String[] key2 = new String[condDimensions.length];
         for (int i = 0; i < condDimensions.length; i++) {
             Object value = getProperty(event, condDimensions[i]);
@@ -85,8 +106,9 @@ public class DimensionService {
             }
             key2[i] = value.toString();
         }
+        //LOGIN_sset_operateIp.devId.mobile_127.0.0.1.xxxxxxx-xxx
         String key = event.getScene() + "_sset_" + key1 + "_" + String.join(".", key2);
-
+        //17712341234
         Object value = getProperty(event, aggrDimension);
         if (value == null || "".equals(value)) {
             return 0;
